1b378ca578d84c5496dec76ec230c2bcc353847d
nicolaslozanoc12 <nicolaslozanoc12@gmail.com>
2024-09-08 10:07:24 -0500

    implementacion de logica en EstudianteDAOImpl

--- src/main/java/org/example/ImplDAO/EstudianteDAOImpl.java
+++ src/main/java/org/example/ImplDAO/EstudianteDAOImpl.java
@@ -1,15 +1,19 @@
 package org.example.ImplDAO;
 
 import org.example.DAO.EstudianteDAO;
-import org.example.DAO.PersonalDAO;
+import org.example.modelo.Direccion;
 import org.example.modelo.Estudiante;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 public class EstudianteDAOImpl implements EstudianteDAO {
-    private PersonalDAOImpl personalDAO = new PersonalDAOImpl();
+    private final PersonalDAOImpl personalDAO = new PersonalDAOImpl();
+    private final DireccionDAOImpl direccionDAO = new DireccionDAOImpl();
     @Override
     public void insertar(Estudiante objeto) {
         try{
@@ -21,7 +25,7 @@ public class EstudianteDAOImpl implements EstudianteDAO {
             preparedStatement.setDouble(4,objeto.getPromedio());
             preparedStatement.executeUpdate();
         }catch (SQLException e){
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "error al buscar", e);
         }
     }
 
@@ -32,21 +36,87 @@ public class EstudianteDAOImpl implements EstudianteDAO {
 
     @Override
     public Estudiante buscarPorId(int id) {
-
+        Estudiante estudiante=null;
+        try {
+            String consulta = "SELECT p.id, p.nombre, p.apellidos, p.id_direccion, e.codigo, e.programa, e.promedio " +
+                    "FROM emb.personales p " +
+                    "JOIN emb.estudiantes e ON p.id = e.id " +
+                    "WHERE p.id = ?";
+            PreparedStatement preparedStatement=conexion.prepareStatement(consulta);
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                int idResult = resultSet.getInt("id");
+                String nombre = resultSet.getString("nombre");
+                String apellido = resultSet.getString("apellidos");
+                int idDireccion = resultSet.getInt("id_direccion");
+                String codigo=resultSet.getString("codigo");
+                String programa=resultSet.getString("programa");
+                Double promedio=resultSet.getDouble("promedio");
+                Direccion direccion=direccionDAO.buscarPorId(idDireccion);
+                estudiante=new Estudiante(idResult,nombre,apellido,direccion,codigo,programa,promedio);
+            }
+        }catch (SQLException e) {
+            logger.log(Level.SEVERE, "error al buscar", e);
+        }
+        return estudiante;
     }
 
     @Override
     public List<Estudiante> buscarTodos() {
-        return null;
+        List<Estudiante> estudiantes= new ArrayList<>();
+        try {
+            String consulta = "SELECT p.id, p.nombre, p.apellidos, p.id_direccion, e.codigo, e.programa, e.promedio " +
+                    "FROM emb.personales p " +
+                    "JOIN emb.estudiantes e ON p.id = e.id ";
+            PreparedStatement preparedStatement=conexion.prepareStatement(consulta);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int idResult = resultSet.getInt("id");
+                String nombre = resultSet.getString("nombre");
+                String apellido = resultSet.getString("apellidos");
+                int idDireccion = resultSet.getInt("id_direccion");
+                String codigo=resultSet.getString("codigo");
+                String programa=resultSet.getString("programa");
+                Double promedio=resultSet.getDouble("promedio");
+                Direccion direccion=direccionDAO.buscarPorId(idDireccion);
+                Estudiante estudiante=new Estudiante(idResult,nombre,apellido,direccion,codigo,programa,promedio);
+                estudiantes.add(estudiante);
+            }
+        }catch (SQLException e) {
+            logger.log(Level.SEVERE, "error al buscar", e);
+        }
+        return estudiantes;
     }
 
     @Override
     public void actualizar(Estudiante objeto, int id) {
+        try {
+            personalDAO.actualizar(objeto,id);
+            PreparedStatement ps = conexion.prepareStatement("UPDATE emb.estudiantes SET codigo = ?, programa = ?, promedio = ? WHERE id = ?");
+            ps.setString(1, objeto.getCodigo());
+            ps.setString(2, objeto.getPrograma());
+            ps.setDouble(3, objeto.getPromedio());
+            ps.setInt(4, objeto.getId());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "error al buscar", e);
+        }
 
     }
 
     @Override
     public void eliminar(int id) {
+        try {
+            String sqlEstudiante = "DELETE FROM emb.estudiantes WHERE id = ?";
+            PreparedStatement psEstudiante = conexion.prepareStatement(sqlEstudiante);
+            psEstudiante.setInt(1, id);
+            psEstudiante.executeUpdate();
+            personalDAO.eliminar(id);
 
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "error al buscar", e);
+        }
     }
 }
--- src/main/java/org/example/modelo/Empleado.java
+++ src/main/java/org/example/modelo/Empleado.java
@@ -3,27 +3,23 @@ package org.example.modelo;
 public class Empleado extends Persona {
     private Cargo cargo;
     private Double salario;
-    private Persona persona;
-    private int id_persona;
 
-    public Empleado(String nombre, String apellidos, Direccion direccion) {
-        super(nombre, apellidos, direccion);
+
+    public Empleado(int id,String nombre, String apellidos, Direccion direccion,Cargo cargo, Double salario) {
+        super(id,nombre, apellidos, direccion);
+        this.cargo = cargo;
+        this.salario = salario;
     }
     @Override
     public String toString(){
         return super.toString()+ cargo + "salario:" + salario;
     }
 
-    public int getId_persona() {
-        return id_persona;
-    }
 
     public Cargo getCargo() {
         return cargo;
     }
-    public Persona getPersona() {
-        return persona;
-    }
+
     public Double getSalario() {
         return salario;
     }
